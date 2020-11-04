8ebdad3527d393cb5984ccae020e1673c30c278e
54481andrew <abasi684@gmail.com>
2020-11-04 09:33:54 -0600

    updated Prep_Pathogen_Data.r

--- Pathogen_Layer/Prep_Pathogen_Data.r
+++ Pathogen_Layer/Prep_Pathogen_Data.r
@@ -4,44 +4,45 @@
 ## environmental predictors that are used to train the pathogen layer of
 ## the model.
 
-Prep.Pathogen.Data <- function(){
-
+Prep.Pathogen.Data <- function(hypers.i){
+    
     ## Load in Lassa survey data-set
-    full.dat <- read.csv(file = 'Data/Lassa_Occurrence_Data_v10_WB.csv', stringsAsFactors = FALSE,
+    full.dat <- read.csv(file = 'Data/Cleaned_Lassa_Literature.csv', stringsAsFactors = FALSE,
                          sep = '\t')
     
-    ## combine Genus and Species columns into single column Species (v10)
+    ## Combine Genus and Species columns into single column Species (v10)
     full.dat$Species = with(full.dat, paste(Genus, Species, sep = ' '))
-        
-    ## Calculate proportion infected and proportion seropositive for all entries
-    full.dat$PropAg <- full.dat$NumPosAg/full.dat$NumTestAg
-    full.dat$PropAb <- full.dat$NumPosAb/full.dat$NumTestAb
     
+    ## Calculate proportion infected and proportion seropositive for all entries
+    full.dat$PropVirus <- full.dat$NumPosVirus/full.dat$NumTestVirus
+    full.dat$PropAb <- full.dat$NumPosAb/full.dat$NumTestAb
+
+    full.dat$Human_Random_Survey = full.dat$Species== 'Homo sapiens'
+
     ## Keep only those columns that are needed, and discard any entries
     ## without Lat/Lon values. This dataset is called classi.dat, short
     ## for classification dataset.
     full.dat <- full.dat[,c('Longitude', 'Latitude', 'Country', 'Village',
-                            'Species', 'NumPosAg', 'NumTestAg', 'PropAg',
-                            'NumPosAb', 'NumTestAb', 'PropAb', 'Source',
-                            'DiagnosticMethod', 'Target', 'Human_Random_Survey')]
+                            'Species', 'NumPosVirus', 'NumTestVirus', 'PropVirus',
+                            'Virus_Diagnostic_Method',
+                            'NumPosAb', 'NumTestAb', 'PropAb',
+                            'Ab_Diagnostic_Method', 'Antibody_Target',
+                            'Source',
+                            'Human_Random_Survey',
+                            'Year', 'Bibtex')]
+                            
     keep <- !(is.na(full.dat$Latitude) | is.na(full.dat$Longitude))
     classi.dat <- full.dat[keep,]
     
     ## Add columns describing total individuals tested (TotTest), total
-    ## number of individuals that were arenavirus positive (TotPos),
-    ## and binary ArenaStat column indicating whether Lassa virus
-    ## was found (1) or not (0)
-    classi.dat = add.total.columns(classi.dat)
-    
-    ## If the ArenaStat column is neither positive or negative, there is
-    ## no information. Remove rows with no information.
-    classi.dat <- classi.dat[which(!is.na(classi.dat$ArenaStat |
-                                          classi.dat$Species=='Homo sapiens')),]
+    ## number of individuals that were arenavirus positive (TotPos).
+    classi.dat = add.total.columns(classi.dat, hypers.i$min.test)
+    classi.dat$genbank <- FALSE
     
     ## -- Incorporate GenBank dataset into classi.dat
     
     ## Load GenBank data
-    genbank.dat <- read.csv('Data/Rodents_Genbank_June16_2020.csv', stringsAsFactors = FALSE,
+    genbank.dat <- read.csv('Data/Rodents_Genbank_Oct_2020.csv', stringsAsFactors = FALSE,
                          sep = '\t')
     
     ## Prune the dataset to those entries that are of Mastomys natalensis, and
@@ -55,13 +56,19 @@ Prep.Pathogen.Data <- function(){
     genbank.dat.form <- with(genbank.dat,
                              data.frame(Longitude = Long, Latitude = Lat,
                                         Country = Country, Village = LocVillage,
-                                        Species = Species, NumPosAg = 1,
-                                        NumTestAg = 1, PropAg = NA,
+                                        Species = Species, NumPosVirus = 1,
+                                        NumTestVirus = 1, PropVirus = NA,
+                                        Virus_Diagnostic_Method = 'PCR',
                                         NumPosAb = 0, NumTestAb = 0,
-                                        PropAb = NA, Source = Reference,
-                                        DiagnosticMethod = 'Various', Target = 'Ag',
+                                        PropAb = NA,
+                                        Ab_Diagnostic_Method = NA,
+                                        Antibody_Target = NA,
+                                        Source = Reference,
                                         Human_Random_Survey= FALSE,
-                                        ArenaStat = 1, TotPos = 1, TotTest = 1
+                                        Year = gbCollectYear,
+                                        Bibtex = NA,
+                                        TotPos = 1, TotTest = 1,
+                                        genbank = TRUE
                                         ))
     
     ## Add into classi.dat
@@ -84,8 +91,9 @@ Prep.Pathogen.Data <- function(){
     classi.dat.jrod <- classi.dat[classi.dat$Species=='Mastomys natalensis', ]
     human.test.dat <-  classi.dat[classi.dat$Human_Random_Survey==TRUE &
                                   classi.dat$NumTestAb > 0, ]
+    ## Add ArenaStat column based on serology. This is only used for graphing. 
     human.test.dat$ArenaStat <- 1*(human.test.dat$NumPosAb > 0)
-    
+
     
     ## Aggregate data that fall within unique pixels. This is
     ## achieved with the purge.repeats function. The pixelation of
@@ -94,6 +102,62 @@ Prep.Pathogen.Data <- function(){
     template.rast <- all.stack[['Pmu']]
     purged.out <- purge.repeats(classi.dat.jrod, template.rast)
     jrod.purged <- purged.out[[1]]
+
+
+    ## Keep track of the number of pixels that are 
+    ## ambiguous given our definition of LASV +. Column names refer to the
+    ## literature data except for those with suffix y refers to genbank.
+    ##These are pixels in which not enough rodents were tested, or were serology-only.
+    classi.dat.jrod$Cell <- raster::extract(template.rast,
+                                            classi.dat.jrod[,c('Longitude','Latitude')],
+                                            cellnumbers=TRUE)[,'cells']
+    rod.lit <- classi.dat.jrod[classi.dat.jrod$genbank==FALSE,]
+    rod.gen <- classi.dat.jrod[classi.dat.jrod$genbank==TRUE,]
+    rod.gen <- aggregate(NumPosVirus~Cell,rod.gen, FUN = sum)
+    rod.lit.ambiguous <- rod.lit[(rod.lit$NumPosAb > 0 &
+                                            rod.lit$NumPosVirus==0),]
+
+    ## Merge genbank information into those entries that are ambiguous
+    merged.ambiguous <- merge(rod.lit.ambiguous, rod.gen[,c('NumPosVirus','Cell')], by = 'Cell',
+                              all.x = TRUE, all.y = FALSE)
+    merged.ambiguous <- merged.ambiguous[,c('Cell','Source', 'Village', 'Country',
+                                            'NumPosVirus.x', 'NumTestVirus',
+                                            'NumPosAb', 'NumTestAb',
+                                            'NumPosVirus.y')]
+    merged.ambiguous.data <- merged.ambiguous[order(merged.ambiguous$NumPosVirus.y),]
+
+    ## Pixels for which no genbank data was found have NumPosVirus.y= NA; set these to 0
+    merged.ambiguous.data[is.na(merged.ambiguous.data$NumPosVirus.y),'NumPosVirus.y'] <- 0
+    ## Points with confirmed LASV virus from genbank are not ambiguous, so remove them
+    ambiguous.data <- merged.ambiguous.data[merged.ambiguous.data$NumPosVirus.y==0,]
+
+## -----------------
+    
+    ## Define sites as positive or negative or NA for arenavirus (indicated in ArenaStat column).
+    
+    ## Three kinds of sites are negative (meet minimum # tested, all tested are negative),
+    ## positive (1 or more confirmed LASV infection), and ambiguous (positive serology
+    ## without virus explicitly detected). Ambiguous sites are set
+    ## according to set.ambiguous.sites option in hypers.dat
+    jrod.purged$ArenaStat <- NA
+    mask.neg <- jrod.purged$TotTest >= hypers.i$min.test & jrod.purged$NumPosVirus ==0 &
+        jrod.purged$NumPosAb ==0
+    mask.pos <- jrod.purged$NumPosVirus > 0
+    mask.ambi <- jrod.purged$NumPosAb > 0 & jrod.purged$NumPosVirus==0
+        
+    jrod.purged[mask.pos, 'ArenaStat'] <- 1
+    jrod.purged[mask.neg, 'ArenaStat'] <- 0
+    jrod.purged[mask.ambi, 'ArenaStat'] <- hypers.i$set.ambiguous.to
+
+    ## Collect NA values again for removal. Save the sites that have been left ambiguous (NA)
+    mask.na <- is.na(jrod.purged$ArenaStat)
+
+    ## Save ambiguous pixels for debugging purposes
+    ambiguous.pixels <- jrod.purged[mask.ambi,c()]
+    
+    ## Exclude any entries from jrod.purged that still have NA ArenaStat
+    jrod.purged <- jrod.purged[!mask.na,]
+
     
     ## Change coordinates to be at cell center
     orig.points <- cbind(jrod.purged$Longitude, jrod.purged$Latitude)
@@ -110,17 +174,21 @@ Prep.Pathogen.Data <- function(){
     ## Impute any missing values with median
     var.names <- names(all.stack)
     jrod.purged <- impute(jrod.purged, var.names)
-    
+
     ## Save a copy of rodent presence/absence dataset, as well as the human seroprevalence data
-    write.csv(jrod.purged, file = 'Data/Prepped_Pathogen_PresAbs_Data.csv', row.names = FALSE)
-    write.csv(human.test.dat, file = 'Data/Prepped_Human_Seroprevalence_Data.csv', row.names = FALSE)
+    write.csv(jrod.purged, file = paste0('Figures_Fits/', prefix, '/',fold,'/',
+                                         'Prepped_Pathogen_PresAbs_Data.csv'), row.names = FALSE)
+    write.csv(ambiguous.data, file = paste0('Figures_Fits/', prefix, '/',fold,'/',
+                                         'Prepped_Pathogen_Ambiguous_Data.csv'), row.names = FALSE)
+    write.csv(ambiguous.pixels, file = paste0('Figures_Fits/', prefix, '/',fold,'/',
+                                         'Prepped_Pathogen_Ambiguous_Pixels.csv'), row.names = FALSE)
+    write.csv(human.test.dat, file = paste0('Figures_Fits/', prefix, '/',fold,'/',
+                                            'Prepped_Human_Seroprevalence_Data.csv'),
+              row.names = FALSE)
     
     cat('--Lassa Rodent Count Statistics--')
     tab <- table(jrod.purged$ArenaStat)
     print(addmargins(tab, FUN = list(Total = sum), quiet = TRUE))
-    ## --Lassa Rodent Count Statistics--
-    ##     0     1 Total
-    ##    36    38    74
     
     ## Number of seroprevalence studies of humans
     print(paste('Number of Human Sources: ', length(unique(human.test.dat$Source))), quote = FALSE)
@@ -129,19 +197,17 @@ Prep.Pathogen.Data <- function(){
     print(paste('Number of Countries: ', length(unique(human.test.dat$Country))), quote = FALSE)
     
     ## Time span
-    temp <- with(human.test.dat, strsplit(paste(Source), '_'))
-    years = sapply(temp, FUN = function(x){x[length(x)]})
-    years = as.numeric(years)
-    years = years[!is.na(years)] ## Filter out entries that are not years
+    temp <- with(human.test.dat, strsplit(paste(Year), '-'))
+    years = sapply(temp, FUN = function(x){range(as.numeric(x))})
     print(paste0('Human seroprevalence collected between ', paste(range(years), collapse = '-')), quote = FALSE)
-    ##"Human seroprevalence collected between 1973-2019"
     
-    png(file = paste0('Figures_Fits/Human_Test_Dat.png'), width = 6, height = 4, units = 'in', res = 400)
+    png(file = paste0('Figures_Fits/', prefix, '/',fold,'/',
+                      'Human_Test_Dat.png'), width = 6, height = 4, units = 'in', res = 400)
     par(mai = 1*c(0.2,0.2,0.2,0.2))
     plot(foc.shp.ogr, col = 'cornsilk')
     points(Latitude~Longitude, human.test.dat, bg = 'green', col = 'black', pch = 21, cex = 0.5)
     legend(x = 'topleft', legend = 'Human LASV sero-survey', pch = 21, pt.bg = 'green', col = 'black')
     dev.off()
     
-    return(jrod.purged)
+    return(list(jrod.purged, ambiguous.data, ambiguous.pixels))
 } ## End function    
